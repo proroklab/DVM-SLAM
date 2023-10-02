80ae00d20eed4229c6fe0f82add64fd1d340035a
jyjblrd <jyjbird@gmail.com>
2023-10-03 00:18:05 +0100

    build.sh compile with debug info

--- src/orb_slam3_ros/orb_slam3/build.sh
+++ src/orb_slam3_ros/orb_slam3/build.sh
@@ -3,7 +3,7 @@ echo "Configuring and building Thirdparty/DBoW2 ..."
 cd Thirdparty/DBoW2
 mkdir build
 cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release
+cmake .. -DCMAKE_BUILD_TYPE=Debug
 make -j
 
 cd ../../g2o
@@ -12,7 +12,7 @@ echo "Configuring and building Thirdparty/g2o ..."
 
 mkdir build
 cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release
+cmake .. -DCMAKE_BUILD_TYPE=Debug
 make -j
 
 cd ../../Sophus
@@ -21,7 +21,7 @@ echo "Configuring and building Thirdparty/Sophus ..."
 
 mkdir build
 cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release
+cmake .. -DCMAKE_BUILD_TYPE=Debug
 make -j
 
 cd ../../../
@@ -32,9 +32,9 @@ cd Vocabulary
 tar -xf ORBvoc.txt.tar.gz
 cd ..
 
-echo "Configuring and building ORB_SLAM3 ..."
-
-mkdir build
-cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release
-make -j4
+#echo "Configuring and building ORB_SLAM3 ..."
+#
+#mkdir build
+#cd build
+#cmake .. -DCMAKE_BUILD_TYPE=Debug
+#make -j4
