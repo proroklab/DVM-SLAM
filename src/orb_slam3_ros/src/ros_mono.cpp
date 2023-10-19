ca090e5615a1f8c5b1293ec96896e6ceb593a6a2
jyjblrd <jyjbird@gmail.com>
2023-10-19 17:25:14 +0100

    idk working ish

--- src/orb_slam3_ros/src/ros_mono.cpp
+++ src/orb_slam3_ros/src/ros_mono.cpp
@@ -1,3 +1,4 @@
+#include "System.h"
 #include "cv_bridge/cv_bridge.h"
 #include "opencv2/highgui/highgui.hpp"
 #include "rclcpp/rclcpp.hpp"
@@ -9,20 +10,33 @@ class ImageGrabber : public rclcpp::Node {
 public:
   ImageGrabber() : Node("image_grabber") {
     image_subscriber_ = this->create_subscription<sensor_msgs::msg::Image>(
-        "/Mavic_3_PRO/camera/image_color", 1,
-        bind(&ImageGrabber::grab_image, this, placeholders::_1));
+        "/Mavic_2_PRO/camera/image_color", 1,
+        std::bind(&ImageGrabber::grab_image, this, std::placeholders::_1));
+
+    pSLAM = new ORB_SLAM3::System(voc_file, settings_file,
+                                  ORB_SLAM3::System::MONOCULAR, true);
   };
 
 private:
   rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr image_subscriber_;
+  ORB_SLAM3::System *pSLAM;
+
+  string voc_file =
+      "/home/joshuabird/Desktop/Parallels\ Shared\ Folders/ubuntuSharedFolder/"
+      "part_II_project/src/orb_slam3_ros/orb_slam3/Vocabulary/ORBvoc.txt";
+  string settings_file = "/home/joshuabird/Desktop/Parallels\ Shared\ Folders/"
+                         "ubuntuSharedFolder/part_II_project/src/orb_slam3_ros/"
+                         "orb_slam3/Examples/Monocular/EuRoC.yaml";
 
   void grab_image(const sensor_msgs::msg::Image::SharedPtr msg) {
     try {
       cv_bridge::CvImagePtr cv_ptr =
           cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
-      // Do something with the image (cv_ptr->image)
-      cv::imshow("Image Viewer", cv_ptr->image);
-      cv::waitKey(1);
+
+      rclcpp::Time timestamp = msg->header.stamp;
+      double seconds = timestamp.nanoseconds() * 1.e-9;
+      Sophus::SE3f Tcw = pSLAM->TrackMonocular(cv_ptr->image, seconds);
+
     } catch (cv_bridge::Exception &e) {
       RCLCPP_ERROR(this->get_logger(), "Could not convert from '%s' to 'bgr8'.",
                    msg->encoding.c_str());
