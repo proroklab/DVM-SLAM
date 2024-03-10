eea3f942f9b032a90f9503b899f2e7d2230c09b2
jyjblrd <jyjbird@gmail.com>
2024-03-10 21:43:23 +0000

    draw all map points

--- src/orb_slam3_ros/orb_slam3/src/MapDrawer.cc
+++ src/orb_slam3_ros/orb_slam3/src/MapDrawer.cc
@@ -128,36 +128,20 @@ void MapDrawer::DrawMapPoints() {
     return;
 
   const vector<MapPoint*>& vpMPs = pActiveMap->GetAllMapPoints();
-  const vector<MapPoint*>& vpRefMPs = pActiveMap->GetReferenceMapPoints();
-
-  set<MapPoint*> spRefMPs(vpRefMPs.begin(), vpRefMPs.end());
 
   if (vpMPs.empty())
     return;
 
   glPointSize(mPointSize);
   glBegin(GL_POINTS);
-  glColor3f(0.0, 0.0, 0.0);
+  glColor3f(1.0, 0.0, 0.0);
 
   for (size_t i = 0, iend = vpMPs.size(); i < iend; i++) {
-    if (vpMPs[i]->isBad() || spRefMPs.count(vpMPs[i]))
+    if (vpMPs[i]->isBad())
       continue;
     Eigen::Matrix<float, 3, 1> pos = vpMPs[i]->GetWorldPos();
     glVertex3f(pos(0), pos(1), pos(2));
   }
-  glEnd();
-
-  glPointSize(mPointSize);
-  glBegin(GL_POINTS);
-  glColor3f(1.0, 0.0, 0.0);
-
-  for (set<MapPoint*>::iterator sit = spRefMPs.begin(), send = spRefMPs.end(); sit != send; sit++) {
-    if ((*sit)->isBad())
-      continue;
-    Eigen::Matrix<float, 3, 1> pos = (*sit)->GetWorldPos();
-    glVertex3f(pos(0), pos(1), pos(2));
-  }
-
   glEnd();
 }
 
