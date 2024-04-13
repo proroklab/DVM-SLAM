d7516451b7b0a4bd6a4357cbad5d83c1b85175c0
jyjblrd <jyjbird@gmail.com>
2024-04-13 20:06:37 +0100

    allow compressed images

--- src/orb_slam3_ros/src/ros_mono.cpp
+++ src/orb_slam3_ros/src/ros_mono.cpp
@@ -24,12 +24,21 @@ public:
     this->declare_parameter("reliableImageTransport", true);
     bool reliableImageTransport = this->get_parameter("reliableImageTransport").as_bool();
 
-    image_subscriber_thread = std::thread([this, imageTopic, reliableImageTransport]() {
+    this->declare_parameter("compressedImage", true);
+    bool compressedImage = this->get_parameter("compressedImage").as_bool();
+
+    image_subscriber_thread = std::thread([this, imageTopic, reliableImageTransport, compressedImage]() {
       auto sub_node = rclcpp::Node::make_shared("image_subscriber_thread_node");
-      image_subscriber = sub_node->create_subscription<sensor_msgs::msg::Image>(imageTopic,
-        reliableImageTransport ? RELIABLE_QOS : BEST_EFFORT_QOS,
-        std::bind(&OrbSlam3Mono::grab_image, this, std::placeholders::_1));
-      rclcpp::spin(sub_node);
+      if (compressedImage) {
+        compressed_image_subscriber = sub_node->create_subscription<sensor_msgs::msg::CompressedImage>(imageTopic,
+          reliableImageTransport ? RELIABLE_QOS : BEST_EFFORT_QOS,
+          std::bind(&OrbSlam3Mono::grab_compressed_image, this, std::placeholders::_1));
+      }
+      else {
+        image_subscriber = sub_node->create_subscription<sensor_msgs::msg::Image>(imageTopic,
+          reliableImageTransport ? RELIABLE_QOS : BEST_EFFORT_QOS,
+          std::bind(&OrbSlam3Mono::grab_image, this, std::placeholders::_1));
+      }
     });
 
     run();
@@ -37,6 +46,7 @@ public:
 
 private:
   rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr image_subscriber;
+  rclcpp::Subscription<sensor_msgs::msg::CompressedImage>::SharedPtr compressed_image_subscriber;
   rclcpp::TimerBase::SharedPtr timer_;
   std::thread image_subscriber_thread;
 
@@ -49,20 +59,38 @@ private:
       cv::Mat image_cpy = cv_ptr->image.clone();
 
       rclcpp::Time timestamp = msg->header.stamp;
-      double seconds = timestamp.nanoseconds() * 1.e-9;
-      RCLCPP_INFO(this->get_logger(), "New Image. Timestamp: %f", seconds);
-      Sophus::SE3f Tcw = pSLAM->TrackMonocular(image_cpy, seconds);
-
-      rclcpp::Time msg_time = msg->header.stamp;
-
-      publishRosVizTopics->publish_camera_pose(Tcw.inverse(), msg_time);
-
-      newFrameProcessed = true;
-      lastFrameTimestamp = msg_time;
+      process_image(image_cpy, timestamp);
 
     } catch (cv_bridge::Exception& e) {
       RCLCPP_ERROR(this->get_logger(), "Could not convert from '%s' to 'bgr8'.", msg->encoding.c_str());
     }
+  }
+
+  void grab_compressed_image(const sensor_msgs::msg::CompressedImage::SharedPtr msg) {
+    try {
+      cv_bridge::CvImagePtr cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+
+      // image must be copied since it uses the conversion_mat_ for storage
+      // which is asynchronously overwritten in the next callback invocation
+      cv::Mat image_cpy = cv_ptr->image.clone();
+
+      rclcpp::Time timestamp = msg->header.stamp;
+      process_image(image_cpy, timestamp);
+
+    } catch (cv_bridge::Exception& e) {
+      RCLCPP_ERROR(this->get_logger(), "Could not convert from '%s' to 'bgr8'.", msg->format.c_str());
+    }
+  }
+
+  void process_image(const cv::Mat& image, const rclcpp::Time& timestamp) {
+    double seconds = timestamp.nanoseconds() * 1.e-9;
+    RCLCPP_INFO(this->get_logger(), "New Image. Timestamp: %f", seconds);
+    Sophus::SE3f Tcw = pSLAM->TrackMonocular(image, seconds);
+
+    publishRosVizTopics->publish_camera_pose(Tcw.inverse(), timestamp);
+
+    newFrameProcessed = true;
+    lastFrameTimestamp = timestamp;
   }
 };
 
