9d35da404e8ce847b2c0689ef2852bcc71a832ac
jyjblrd <jyjbird@gmail.com>
2024-05-02 16:38:32 +0100

    rename to central_management_interface

--- run.bash
+++ run.bash
@@ -7,7 +7,7 @@ options=()
 
 cmds[1]="source install/setup.bash && source /home/joshuabird/Documents/webots_ros2/install/setup.bash && ros2 launch webots_sim robot_launch.py"
 
-cmds[2]="source install/setup.bash && ros2 run controller my_node"
+cmds[2]="source install/setup.bash && ros2 run central_management_interface my_node"
 
 cmds[3]="source install/setup.bash && ros2 run slam_system ros_mono --ros-args -p agentId:=1 -p config:=/home/joshuabird/Desktop/orbslam_configs/configs/euroc.yaml"
 
